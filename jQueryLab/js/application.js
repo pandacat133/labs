3b5c941befddcc1e88ee4ada086eefa536c549cb
Celie Baker <celie.n133@gmail.com>
2018-02-21 12:33:04 -0700

    DOM traversal examples

--- jQueryLab/js/application.js
+++ jQueryLab/js/application.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function () {
-    $("h1").text("Where to?");
+
+    //SEARCHING THE DOM:
+
+    //descendant selector
+    $("#destinations li");
+    //same as above but using traversal language
+    $("#destinations").find("li");
+
+    //first descendant selector
+    $("li:first");
+    //same as above but using traversal language
+    $("li").first();
+
+
+    //TRAVERSING THE DOM:
+    
+    //walking the DOM -- method chaining
+    $("li").first().next();
+    //walking UP the DOM
+    $("li").first().parent();
+    //walking DOWN the DOM
+    $("#destinations").children("li");
 });
